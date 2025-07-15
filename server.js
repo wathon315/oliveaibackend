4e0a24ff13338e84bb5d23a296d1bf8d4276cf58
wathon315 <wathonnadisan2021@gmail.com>
2025-07-15 14:57:42 +0400

    Update server.js 111

--- server.js
+++ server.js
@@ -354,15 +354,34 @@ mongoose.connect(MONGODB_URI)
 
 // ===== EXPRESS MIDDLEWARE =====
 app.use(cors({
-    origin: [
-        'http://localhost:3000',
-        'http://localhost:5173',
-        'https://wathon315.github.io/oliveai/',  
-    ],
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        
+        const allowedOrigins = [
+            'http://localhost:3000',
+            'http://localhost:5173',
+            'http://localhost:5000',
+            'https://wathon315.github.io',
+            'https://oliveaibackend.onrender.com'
+        ];
+        
+        // Check if the origin is in our allowed list
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            // For development, you might want to allow all origins
+            // callback(null, true);
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+    exposedHeaders: ['Content-Length', 'X-Content-Type-Options'],
+    maxAge: 86400 // 24 hours
 }));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
